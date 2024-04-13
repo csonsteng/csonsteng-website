4d8868eda0ad6256a86f293c6a592c8f41ca8541
Chloe Sonsteng <chloe@csonsteng.com>
2024-04-13 12:25:08 -0700

    adding some default colors

---
+++ src/WebHeader.jsx
@@ -0,0 +1,9 @@
+const WebHeader = () => {
+    return (
+        <div className='header'>
+            <title className='nameHeader'>Chloe Sonsteng</title>
+            
+        </div>
+    )
+}
+export default WebHeader;
