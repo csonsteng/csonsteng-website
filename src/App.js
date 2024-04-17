3b10b6c4b9beea954c83f9036da02c562aa763ff
Chloe Sonsteng <chloe@csonsteng.com>
2024-04-16 22:07:23 -0700

    resizeable windows

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import ResizeableBox from './ResizeableBox';
 
 function App() {
   return (
-    <div className="App">
-      <WebHeader />
-      <ResizeableBox />
+    <div className="App" style={{
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <WebHeader 
+      style={{
+      height: '75px',
+      width: '100%'
+    }}/>
+      <ResizeableBox horizontal={true} element1MinSize={0.1} element2MinSize={0.4}
+      style={{
+        flex: '1',
+        width: '100%'
+      }}/>
     </div>
   );
 }
--- src/ResizeableBox.jsx
+++ src/ResizeableBox.jsx
@@ -1,9 +1,20 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
-const ResizeableBox = ({resizeCallback}) => {
+const ResizeableBox = ({horizontal, element1MinSize, element2MinSize}) => {
 
+    const refContainer = useRef();
     const [isMouseOver, setIsMouseOver] = useState(false);
     const [isDragging, setIsDragging] = useState(false);
+    const [handlePosition, setHandlePosition] = useState('50%');
+    const [size, setSize] = useState({
+        width: 0,
+        height: 0
+    });
+    const [offset, setOffset] = useState({
+        left: 0,
+        top: 0
+    });
+
     function onMouseDown() {
         if(!isMouseOver) return;
         
@@ -11,10 +22,6 @@ const ResizeableBox = ({resizeCallback}) => {
         console.log(`down`);  
     }
 
-    function onMouseUp(event){
-        setIsDragging(false);
-        console.log(`up`);
-    }
 
     function onMouseEnter(){
         setIsMouseOver(true);
@@ -26,26 +33,93 @@ const ResizeableBox = ({resizeCallback}) => {
         console.log(`exit`);
         
     }
-    function onMouseMove(event){
-        if(!isDragging) return;
-        console.log(`dragging ${isDragging} ${isMouseOver}`);
-    }
+
+    useEffect(() => {
+        if (refContainer.current) {
+            const boundingRect = refContainer.current.getBoundingClientRect();
+            setOffset({
+                left: boundingRect.top,
+                top: boundingRect.left
+
+            });
+            setSize({
+                width: boundingRect.width,
+                height: boundingRect.height
+
+            });
+            console.log(`${boundingRect.top} ${boundingRect.width}`)
+        }   
+    }, []);
+
     useEffect(() => {  
+
+        function onMouseMove(event) {
+            if(!isDragging) return;
+
+            let relative = horizontal ? 
+                            (event.clientX - offset['top']) / size['width'] :
+                            (event.clientY - offset['left']) / size['height'];
+            relative = Math.max(relative, element1MinSize);
+            relative = Math.min(relative, 1 - element2MinSize);
+            const handle = `${Math.round(relative * 100)}%`;
+            setHandlePosition(handle);
+            console.log(handle)
+
+        }
+        function onMouseUp(event){
+            setIsDragging(false);
+            console.log(`up`);
+        }
+
+
         window.addEventListener('mousemove', onMouseMove);
         window.addEventListener('mouseup', onMouseUp)
         return () => {
             window.removeEventListener('mousemove', onMouseMove);
             window.removeEventListener('mouseup', onMouseUp)
         };
-    }, [isDragging, isMouseOver]);
+    }, [isDragging, isMouseOver, offset, size]);
 
     return (
         <div className='resize'
         onPointerEnter={onMouseEnter}
         onPointerLeave={onMouseExit}
         onMouseDown={onMouseDown}
-        onmou>
-            poopy
+        ref={refContainer}
+        style={{
+            display: 'flex',
+            height: '100%',
+            width: '100%',
+            alignContents: 'stretch',
+            flexDirection: horizontal ? 'row' : 'column'}}>
+
+            { horizontal ? (
+            <>
+                <div style={{backgroundColor: '#555555', width: handlePosition}}>
+                </div>
+                <div 
+                    onPointerEnter={onMouseEnter}
+                    onPointerLeave={onMouseExit}
+                    onMouseDown={onMouseDown}
+                    style={{backgroundColor: '#000000', width: '15px'}} />
+                <div style={{backgroundColor: '#FFFFFF', flex: '1'}}>
+                    {handlePosition}
+                </div>
+            </>
+        ) : 
+        <>
+            <div style={{backgroundColor: '#555555', height: handlePosition}}>
+            </div>
+            <div 
+                onPointerEnter={onMouseEnter}
+                onPointerLeave={onMouseExit}
+                onMouseDown={onMouseDown}
+                style={{backgroundColor: '#000000', height: '15px'}} />
+            <div style={{backgroundColor: '#FFFFFF', flex: '1'}}>
+                {handlePosition}
+            </div>
+        </>
+        }
         </div>
     )
 }
