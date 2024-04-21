b988f90b60a47f740d216c5d3b4790a419012211
Chloe Sonsteng <chloe@csonsteng.com>
2024-04-20 21:59:03 -0700

    playing w iframes

--- src/ProjectView.jsx
+++ src/ProjectView.jsx
@@ -5,7 +5,20 @@ const ProjectView = () => {
             width: '100%',
             height: '100%',
             backgroundColor: 'green',
-        }}>Show Selected Project Here</div>
+        }}><iframe
+
+        title="poop"
+ 
+        style={{
+            border: "0",
+            height:"100%",
+            width:"100%"}}
+      
+        loading="lazy"   
+        
+        src="https://www.geeksforgeeks.org">
+      
+      </iframe></div>
     )
 }
 
