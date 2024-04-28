0b727e7ba7e9dc9caf42ae12df00946e0360c93b
Chloe Sonsteng <chloe@csonsteng.com>
2024-04-28 11:47:28 -0700

    style cleanup

--- src/ProjectView.jsx
+++ src/ProjectView.jsx
@@ -29,7 +29,6 @@ const ProjectView = ({selectedFile}) => {
         <div style={{
             width: '100%',
             height: '100%',
-            backgroundColor: 'green',
         }}>
             {selectedFile ? showContextForSelectedFile(selectedFile) : null}
         </div>
