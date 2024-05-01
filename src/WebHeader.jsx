3dfc3b95cebd54f11f5be3cbbbb5cb630a7735ce
Chloe Sonsteng <chloe@csonsteng.com>
2024-05-01 01:03:08 -0700

    add contact info to header

--- src/WebHeader.jsx
+++ src/WebHeader.jsx
@@ -10,7 +10,29 @@ const WebHeader = () => {
             alignContent: 'stretch',
             borderBottom: 'solid 3px var(--color-grey-purple)'
         }}>
-            <title className='nameHeader'>Chloe Sonsteng</title>
+            <span style={{
+                  display: 'flex',
+                  fontSize: '1.3em',
+                  color: 'var(--color-soft-purple)',
+                  flexDirection: 'column-reverse',
+                  alignContent: 'baseline',
+                  marginLeft: '5px'
+            }}>Chloe Sonsteng</span>
+            <div style={{
+                height: '90%',
+                display: 'flex',
+                flexDirection: 'row-reverse',
+                alignContent: 'center',
+                alignSelf: 'center',
+                marginRight: '5px'
+            }}>
+                <a  href='https://www.linkedin.com/in/chloe-sonsteng-05477196/' target='_blank' rel="noreferrer" style={{ width: '27px'}}>
+                    <img src='/icons/linkedin.png' alt='linkedin icon' style={{ height: '100%'}}/>
+                </a>
+                <a  href='mailto:chloe@csonsteng.com' target='_blank' rel="noreferrer" style={{ width: '27px'}}>
+                    <img src='/icons/email.png' alt='email icon' style={{ height: '100%'}}/>
+                </a>
+            </div>
             
         </div>
     )
